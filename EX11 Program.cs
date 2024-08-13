95354bb0bed0c3183b1d6ceccc80a9708c83a26c
Libânia SJ <166406038+sjlibania@users.noreply.github.com>
2024-08-13 11:23:30 -0300

    LISTA2EX11

---
+++ EX11 Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA2EX11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+                Console.WriteLine("Digite a nota P1:");
+                double P1 = Convert.ToDouble(Console.ReadLine());
+
+                // Calcula a nota necessária em P2 para que a média seja 5
+                double P2_necessaria = (15 - P1) / 2;
+
+                Console.WriteLine("Para ser aprovado, você precisa tirar " + P2_necessaria + " na P2.");
+    
+            }
+        }
+
+
+    }
+
