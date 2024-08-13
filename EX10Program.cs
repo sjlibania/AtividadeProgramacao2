dabf256ff4921766dc20442018ee2c165bc88540
Libânia SJ <166406038+sjlibania@users.noreply.github.com>
2024-08-13 11:23:01 -0300

    LISTA2EX10

---
+++ EX10Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LISTA2EX10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            
+            
+                Console.WriteLine("Digite a nota P1:");
+                double P1 = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("Digite a nota P2:");
+                double P2 = Convert.ToDouble(Console.ReadLine());
+
+                double media = (P1 + 2 * P2) / 3;
+
+                if (media >= 5)
+                    Console.WriteLine("Aprovado");
+                else
+                    Console.WriteLine("Reprovado");
+
+                Console.WriteLine("Média: " + media);
+            
+        }
+
+    }
+}
+
