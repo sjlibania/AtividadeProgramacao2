caf1f1c5d4c469b8f8f4bc5292be1e7ff03eabf2
Libânia SJ <166406038+sjlibania@users.noreply.github.com>
2024-08-13 11:06:36 -0300

    LISTA2EX4

--- Program.cs
+++ Program.cs
@@ -4,57 +4,35 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LISTA2EX3
+namespace LISTA2EX4
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int a;
             int b;
-            int c;
+            int h;
+            int area;
 
-            Console.Write("Digite o primeiro valor:  ");
-            a = int.Parse(Console.ReadLine());
-            Console.Write("Digite o segundo valor:  ");
+            Console.Write("Digite o valor da base do retângulo:  ");
             b = int.Parse(Console.ReadLine());
-            Console.Write("Digite o terceiro valor:  ");
-            c = int.Parse(Console.ReadLine());
 
-            if (a == b)
-                if (a == c)
-                    Console.WriteLine("Iguais");
-                else
-                    if (a > c)
-                    Console.WriteLine("1°/2°");
-                else
-                    Console.WriteLine("3°");
-            else
-                if (a == c)
-                    if (a > b)
-                       Console.WriteLine("1°/3°");
-                    else
-                    Console.WriteLine("2°");
-           else         
-                if (b == c)
-                    if ( b > a)
-                        Console.WriteLine("2°/3°");
-                     else
-                       Console.WriteLine("1°");
-                else
-                      if (a > b)
-                          if (a > c)
-                             Console.WriteLine("1°");
-                           else
-                             Console.WriteLine("3°");
-                      else
-                          if (b > c)
-                               Console.WriteLine("2°");
-                          else
-                               Console.WriteLine("3°");
+            Console.Write("Digite o valor da altura do retângulo:  ");
+            h = int.Parse(Console.ReadLine());
+
+            area = b * h;
+
+            Console.Write("A area do retângulo é: ");
+            Console.WriteLine(area);
+
+
+            if (area > 100)
+                Console.WriteLine("      Terreno Grande       ");
 
 
 
-        }  
+
+
+        }
     }
 }
