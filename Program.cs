10d1a71fa5036d6f6b98c6f0f806953b1f9ba6ac
Libânia SJ <166406038+sjlibania@users.noreply.github.com>
2024-08-13 11:06:08 -0300

    LISTA2EX3

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace LISTA2EX2
+namespace LISTA2EX3
 {
     internal class Program
     {
@@ -12,29 +12,49 @@ namespace LISTA2EX2
         {
             int a;
             int b;
+            int c;
 
             Console.Write("Digite o primeiro valor:  ");
             a = int.Parse(Console.ReadLine());
-
-
             Console.Write("Digite o segundo valor:  ");
             b = int.Parse(Console.ReadLine());
+            Console.Write("Digite o terceiro valor:  ");
+            c = int.Parse(Console.ReadLine());
 
             if (a == b)
-            {
-                Console.WriteLine("Iguais");
-            }
-            else
-            {
-                if (a > b)
-                {
-                    Console.WriteLine("Primeiro");
-                }
+                if (a == c)
+                    Console.WriteLine("Iguais");
                 else
-                {
-                    Console.WriteLine("Segundo");
-                }
-            }
-        }
+                    if (a > c)
+                    Console.WriteLine("1°/2°");
+                else
+                    Console.WriteLine("3°");
+            else
+                if (a == c)
+                    if (a > b)
+                       Console.WriteLine("1°/3°");
+                    else
+                    Console.WriteLine("2°");
+           else         
+                if (b == c)
+                    if ( b > a)
+                        Console.WriteLine("2°/3°");
+                     else
+                       Console.WriteLine("1°");
+                else
+                      if (a > b)
+                          if (a > c)
+                             Console.WriteLine("1°");
+                           else
+                             Console.WriteLine("3°");
+                      else
+                          if (b > c)
+                               Console.WriteLine("2°");
+                          else
+                               Console.WriteLine("3°");
+
+
+
+        }  
     }
 }
